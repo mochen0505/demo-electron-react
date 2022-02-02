edd005f302dd18b20047ec7597b955663fe8c8f6
Mo Chen <mochen0505@hotmail.com>
2022-02-02 21:47:56 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import { Button } from 'antd'
+import './App.less'
+
+function App() {
+  return (
+      <div>
+        <Button type="primary">test</Button>
+      </div>
+  );
+}
+
+export default App;
